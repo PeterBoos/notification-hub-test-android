4de8f7ae4d3a862703730d8483600bb03dcf72cd
Peter Böös <peterdotboos@gmail.com>
2019-02-06 16:46:31 +0100

    Initial commit of android app

---
+++ com.peterboos.anht/com.peterboos.anht/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace com.peterboos.anht
+{
+    public static class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://boos-sandbox.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=pnTm2hyDHJd6pyd/bZQ7eEP08DVOFbz+Ru6qSs+WRfI=";
+        public const string NotificationHubName = "pb-notify-hub-test";
+    }
+}
---
+++ com.peterboos.anht/com.peterboos.anht/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Util;
+
+namespace com.peterboos.anht
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public const string TAG = "MainActivity";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    if (key != null)
+                    {
+                        var value = Intent.Extras.GetString(key);
+                        Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                    }
+                }
+            }
+        }
+    }
+}
